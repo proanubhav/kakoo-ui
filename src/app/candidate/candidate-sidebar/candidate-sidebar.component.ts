803898078bd660d003f1a4a0a3baf25e69ef5d6e
Anubhav <proanubhav@gmail.com>
2023-06-09 02:06:47 +0530

    sidebar fix

--- src/app/candidate/candidate-sidebar/candidate-sidebar.component.ts
+++ src/app/candidate/candidate-sidebar/candidate-sidebar.component.ts
@@ -7,9 +7,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CandidateSidebarComponent implements OnInit {
 
+  toggleFlag = false;
   constructor() { }
 
   ngOnInit() {
 
+  }
+  toggleMenu() {
+    this.toggleFlag = !this.toggleFlag
   }
 }
